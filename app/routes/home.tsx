278ad9c7ce943253098402487b241b9ad90353eb
READTalk <redtalk.app@hotmail.com>
2026-07-16 20:31:10 +0700

    Refactor home route and update loader function

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,31 +1,17 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
-import { redirect } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "READTalk" },
-    { name: "description", content: "Welcome to READTalk Messenger!" },
-  ];
+	return [
+		{ title: "READTalk" },
+		{ name: "description", content: "Welcome to READTalk Messenger!" },
+	];
 }
 
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const res = await fetch("https://service.readtalk.workers.dev/me", {
-    headers: {
-      "Cookie": request.headers.get("Cookie") ?? ""
-    }
-  });
-
-  if (res.status === 401) {
-    
-    return redirect("https://service.readtalk.workers.dev/");
-  }
-
-  const user = await res.json(); // { id, email }
-  return user;
+export function loader({ context }: Route.LoaderArgs) {
+	return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome user={loaderData} />;
+	return <Welcome message={loaderData.message} />;
 }
