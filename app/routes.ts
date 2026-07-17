a1662c4ebc2636cfd619d66c7effb83e2e65ceb5
READTalk <redtalk.app@hotmail.com>
2026-07-18 01:23:44 +0700

    Delete app/routes.ts

--- app/routes.ts
+++
@@ -1,8 +0,0 @@
-// app/routes.ts //
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("auth/authorize", "routes/auth/authorize.tsx"),
-  route("auth/callback", "routes/auth/callback.tsx"),
-] satisfies RouteConfig;
