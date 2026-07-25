aba1b8470dc6f7b3142c03248d75a2fd545aa6df
READTalk <redtalk.app@hotmail.com>
2026-07-25 19:02:54 +0700

    Update comments and formatting in app.ts

--- workers/app.ts
+++ workers/app.ts
@@ -6,10 +6,10 @@ import authHandler from "./auth";
 
 const app = new Hono();
 
-// IMPORTANT
+// IMPORTANT: OpenAuth harus di atas
 app.route("/auth", authHandler);
 
-// React Router
+// React Router sebagai fallback
 app.all("*", (c) => {
   const requestHandler = createRequestHandler(
     () => import("virtual:react-router/server-build"),
